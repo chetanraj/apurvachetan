ebc5f0b9fa74e50715b961a22605bd49dc2c2747
Chetan Raj <chetan.narian@gmail.com>
2015-05-18 21:03:06 +0530

    landing page changes

--- js/fullPage.js
+++ js/fullPage.js
@@ -1,10 +1,13 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
         anchors: ['home', 'invite', 'location', 'others'],
-        sectionsColor: ['#C63D0F', '#1BBC9B', '#7E8F7C', '#E67E22'],
+        sectionsColor: ['#2EBFEF', '#1BBC9B', '#7E8F7C', '#E67E22'],
         navigation: true,
         navigationPosition: 'right',
         responsive: 1024,
         easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)'
     });
 });
+
+
+// #2EBFEF, #00B6AD, #FFE5B8, #9767AD
