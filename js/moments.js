bfffddef5d88948b3746fcc3e191dbfe13a6e9a3
Chetan Raj <chetan.narian@gmail.com>
2015-05-28 00:51:26 +0530

    moments

--- js/moments.js
+++ js/moments.js
@@ -11,12 +11,27 @@ $.each(urls, function(i,u){
     }).done(function(response) {
         var stream = response.data;
         for(var d = 0; d < stream.length; d++) {
+            var emoArr = ["grin","happy","laugh","saint","squint","tongue","wink","wink2"];
+
+            var rN = getRandomizer( 0, 8 );
+            var emoClass = "emo fontelico-emo-" + emoArr[d] ;
+            var $span = $("<span/>", {"class": emoClass})
+
             var _stream = stream[d];
-            var $view = $("<div/>", {"class":"view"});
-            var $img = $("<img/>", {"src":_stream.images.low_resolution.url, "class":"thumbnail"});            
-            $view.append($img);            
-        
-            $(this).find("#grid").append($view);      
+            var $view = null;
+            if(!$(".view").eq(d).length) {
+                $view = $("<div/>", {"class":"view", "id":d});
+                $(this).find("#grid").append($view);
+            }
+            
+            var $img = $("<img/>", {"src":_stream.images.low_resolution.url, "class":"thumbnail"});               
+            $(".view[id="+d+"]").append($img).append($span);
         }
     });
 });
+
+function getRandomizer(bottom, top) {
+    return function() {
+        return Math.floor( Math.random() * ( 1 + top - bottom ) ) + bottom;
+    }
+}
