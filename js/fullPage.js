8ad6eea71530a8f2c707e2265d67895fc60ff9c6
Chetan Raj <chetan.narian@gmail.com>
2015-06-12 16:41:56 +0530

    app css changes

--- js/fullPage.js
+++ js/fullPage.js
@@ -13,7 +13,18 @@ $(document).ready(function() {
         easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
         fitToSection: true,
     });
+
+    initialize();
 });
+
+function initialize() {
+    var mapCanvas = document.getElementById('map-canvas');
+    var mapOptions = {
+      center: new google.maps.LatLng(12.7960, 77.4963),
+      zoom: 8
+    }
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+}
 
 
 // #2EBFEF, #00B6AD, #FFE5B8, #9767AD
