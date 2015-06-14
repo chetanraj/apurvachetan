f3fefcaf374623e6b87f9f8f40e8415b76200ebe
Chetan Raj <chetan.narian@gmail.com>
2015-06-14 13:47:24 +0530

    for desktop

--- js/fullPage.js
+++ js/fullPage.js
@@ -4,15 +4,22 @@ $(document).ready(function() {
 	  detectDeviceModel: true
 	});
 
-    $('#fullpage').fullpage({
-        sectionsColor: ['#2EBFEF', '', '', ''],
-        navigation: true,
-        navigationPosition: 'right',
-        responsive: 1024,
-        easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
-        fitToSection: true,
-    });
-
+    if($('.desktop').length) {
+        $('#fullpage').fullpage({
+            anchors: ['home', 'invite', 'engaged', 'location'],
+            sectionsColor: ['#2EBFEF', '', '', ''],
+            navigation: true,
+            navigationPosition: 'right',
+            responsive: 1024,
+            easingcss3: 'cubic-bezier(0.175, 0.885, 0.320, 1.275)',
+            fitToSection: true,
+        });
+    } else {
+        $('#fullpage').fullpage({
+            sectionsColor: ['#2EBFEF', '', '', '']
+        });
+    }
+    
     initialize();
 });
 
